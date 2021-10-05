1201ba4e0bf21195436c07003ce6dab5f2ff1d1c
XxMaxFateXx <aleksa.kumric@gmail.com>
2021-10-05 10:04:05 +0200

    Finnished desktop design and fetched all info

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
-const createNode = (elem) => {
-    return document.createElement(elem);
-};
+let form = document.getElementById("form")
 
-// append an element to parent
-const appendNode = (parent, elem) => {
-    parent.appendChild(elem);
-}
+form.addEventListener('submit', function Hello (e) {
+    e.preventDefault()
+   
+    let search = document.getElementById("search").value
 
-// List Element
-const ul = document.querySelector('#users');
+    let originalName = search.split(' ').join('')
 
-// GitHub API URL
-const url = 'https://api.github.com/users';
 
-// make the API call
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        // iterate over users
-        data.map((user) => {
-            // create the elements
-            let li = createNode('li'),
-                img = createNode('img'),
-                span = createNode('span');
-            img.src = user.avatar_url;
-            span.innerText = user.login;
-            // append all elements
-            appendNode(li, img);
-            appendNode(li, span);
-            appendNode(ul, li);
-        });
-    }).catch(err => {
-        console.error('Error: ', err);
-    });
+    fetch("https://api.github.com/users/" + originalName)
+        .then((result) => result.json())
+        .then((data) => {
+
+            let d = new Date(data.created_at)
+            let year = d.getFullYear()
+            let month = d.getMonth()
+            let day = d.getDate()
+
+            let months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+
+
+            document.getElementById('picture').innerHTML = `<img class="picture-css" src="${data.avatar_url}"/>`
+            document.getElementById('joined').innerHTML = `<div class="date-joined">Joined ${day} ${months[month]} ${year}</div>`
+            document.getElementById('name').innerHTML = `<h1 class="name-css">${data.name}</h1>`
+            document.getElementById('username').innerHTML = `<div class="username-css">@${data.login}</div>`
+            document.getElementById('bio').innerHTML = `<div class="bio-css">${data.bio}</div>`
+            document.getElementById('repos').innerHTML = `<h1 class="repos-css">${data.public_repos}</h1>`
+            document.getElementById('followers').innerHTML = `<h1 class="repos-css">${data.followers}</h1>`
+            document.getElementById('following').innerHTML = `<h1 class="repos-css">${data.following}</h1>`
+            document.getElementById('location-text').innerHTML = `<div class="location-text-css">${data.location}</div>`
+            document.getElementById('blog-text').innerHTML = `<div class="blog-text-css">${data.blog}</div>`
+            document.getElementById('twitter-text').innerHTML = `<a href="https://twitter.com/${data.twitter_username}"><div class="twitter-text-css">@${data.twitter_username}</div></a>`
+            document.getElementById('company-text').innerHTML = `<a href="https://github.com/${data.company}"><div class="company-text-css">@${data.company}</div></a>`
+        })
+})
