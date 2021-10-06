4907c12e9a7885a11c359e390cd24d7c04ba3bdc
XxMaxFateXx <aleksa.kumric@gmail.com>
2021-10-06 15:21:59 +0200

    js hotfix v1.1

--- script.js
+++ script.js
@@ -16,8 +16,13 @@ const Fetch = (originalName) => {
                 document.querySelector(".error-message").style.display = "block"
                 return;
             }
-
             updateDateJoined(data)
+
+            const $blog = document.getElementById('blog-text')
+            const $location = document.getElementById('location-text')
+            const $twitter = document.getElementById('twitter-text')
+            const $company = document.getElementById('company-text')
+            
             document.querySelector(".error-message").style.display = "none"
             document.getElementById('picture').innerHTML = `<img class="picture-css" src="${data.avatar_url}"/>`
             document.getElementById('name').innerHTML = `<h1 class="name-css">${data.name ? data.name : data.login}</h1>`
@@ -27,20 +32,20 @@ const Fetch = (originalName) => {
             document.getElementById('repos').innerHTML = `<h1 class="repos-css">${data.public_repos}</h1>`
             document.getElementById('followers').innerHTML = `<h1 class="repos-css">${data.followers}</h1>`
             document.getElementById('following').innerHTML = `<h1 class="repos-css">${data.following}</h1>`
-            document.getElementById('location-text').innerHTML = `<div class="location-text-css">${data.location ? data.location : "Not Available"}</div>`
-            document.getElementById('location-text').style.opacity = `${data.location ? "100%" : "50%"}`
+            $location.innerHTML = `<div class="location-text-css">${data.location ? data.location : "Not Available"}</div>`
+            $location.style.opacity = `${data.location ? "100%" : "50%"}`
             document.querySelector('.img-location').style.opacity = `${data.location ? "100%" : "50%"}`
-            document.getElementById('blog-text').innerHTML = `<a href="${data.blog}" <div class="blog-text-css">${data.blog ? data.blog : "Not Available"}</div></a>`
-            document.getElementById('blog-text').style.opacity = `${data.blog ? "100%" : "50%"}`
-            document.getElementById('blog-text').style.pointerEvents = `${data.blog ? "all" : "none"}`
+            $blog.innerHTML = `<a href="${data.blog}" <div class="blog-text-css">${data.blog ? data.blog : "Not Available"}</div></a>`
+            $blog.style.opacity = `${data.blog ? "100%" : "50%"}`
+            $blog.style.pointerEvents = `${data.blog ? "all" : "none"}`
             document.querySelector('.img-blog').style.opacity = `${data.blog ? "100%" : "50%"}`
-            document.getElementById('twitter-text').innerHTML = `<a href="https://twitter.com/${data.twitter_username}" <div class="twitter-text-css">${data.twitter_username ? data.twitter_username : "Not Available"}</div></a>`
-            document.getElementById('twitter-text').style.pointerEvents = `${data.twitter_username ? "all" : "none"}`
-            document.getElementById('twitter-text').style.opacity = `${data.twitter_username ? "100%" : "50%"}`
+            $twitter.innerHTML = `<a href="https://twitter.com/${data.twitter_username}" <div class="twitter-text-css">${data.twitter_username ? data.twitter_username : "Not Available"}</div></a>`
+            $twitter.style.pointerEvents = `${data.twitter_username ? "all" : "none"}`
+            $twitter.style.opacity = `${data.twitter_username ? "100%" : "50%"}`
             document.querySelector('.img-twitter').style.opacity = `${data.twitter_username ? "100%" : "50%"}`
-            document.getElementById('company-text').innerHTML = data.company ? `<a href="https://github.com/${data.company.slice(1)}" <div class="company-text-css">${data.company}</div></a>` : `<div class="company-text-css">Not Available</div>`
-            document.getElementById('company-text').style.pointerEvents = `${data.company ? "all" : "none"}`
-            document.getElementById('company-text').style.opacity = `${data.company ? "100%" : "50%"}`
+            $company.innerHTML = data.company ? `<a href="https://github.com/${data.company.slice(1)}" <div class="company-text-css">${data.company}</div></a>` : `<div class="company-text-css">Not Available</div>`
+            $company.style.pointerEvents = `${data.company ? "all" : "none"}`
+            $company.style.opacity = `${data.company ? "100%" : "50%"}`
             document.querySelector('.img-company').style.opacity = `${data.company ? "100%" : "50%"}`
         }
     )
