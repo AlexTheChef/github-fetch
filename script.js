99d27db013b65bc0b54043e0b7cd620e041b702e
XxMaxFateXx <aleksa.kumric@gmail.com>
2021-10-06 15:07:18 +0200

    optimized js file

--- script.js
+++ script.js
@@ -1,88 +1,49 @@
+const months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+const url = "https://api.github.com/users/"
+function updateDateJoined(data) {
+    let d = new Date(data.created_at)
+    let year = d.getFullYear()
+    let month = d.getMonth()
+    let day = d.getDate()
+    document.getElementById('joined').innerHTML = `<div class="date-joined">Joined ${day} ${months[month]} ${year}</div>`
+}
+
 const Fetch = (originalName) => {
-    fetch("https://api.github.com/users/" + originalName)
+    fetch(url + originalName)
         .then((result) => result.json())
         .then((data) => {
-
-            let d = new Date(data.created_at)
-            let year = d.getFullYear()
-            let month = d.getMonth()
-            let day = d.getDate()
-
-            let months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
-            if (data.login) {
-                document.querySelector(".error-message").style.display = "none"
-
-                document.getElementById('picture').innerHTML = `<img class="picture-css" src="${data.avatar_url}"/>`
-                document.getElementById('joined').innerHTML = `<div class="date-joined">Joined ${day} ${months[month]} ${year}</div>`
-                if(data.name == null){
-                    document.getElementById('name').innerHTML = `<h1 class="name-css">${data.login}</h1>`
-                }
-                else{
-                    document.getElementById('name').innerHTML = `<h1 class="name-css">${data.name}</h1>`
-                }
-                
-                document.getElementById('username').innerHTML = `<div class="username-css">@${data.login}</div>`
-                if (data.bio == null) {
-                    document.getElementById('bio').innerHTML = `<div class="bio-css">This profile has no bio</div>`
-                    document.getElementById('bio').style.opacity = "50%"
-                }
-                else {
-                    document.getElementById('bio').innerHTML = `<div class="bio-css">${data.bio}</div>`
-                    document.getElementById('bio').style.opacity = "100%"
-                }
-                document.getElementById('repos').innerHTML = `<h1 class="repos-css">${data.public_repos}</h1>`
-                document.getElementById('followers').innerHTML = `<h1 class="repos-css">${data.followers}</h1>`
-                document.getElementById('following').innerHTML = `<h1 class="repos-css">${data.following}</h1>`
-
-                if (data.location == null) {
-                    document.getElementById('location-text').innerHTML = `<div class="location-text-css">Not Available</div>`
-                    document.getElementById('location-text').style.opacity = "50%"
-                    document.querySelector(".img-location").style.opacity = "50%"
-                }
-                else {
-                    document.getElementById('location-text').innerHTML = `<div class="location-text-css">${data.location}</div>`
-                    document.getElementById('location-text').style.opacity = "100%"
-                    document.querySelector(".img-location").style.opacity = "100%"
-                }
-
-                if (data.blog == '') {
-                    document.getElementById('blog-text').innerHTML = `<div class="blog-text-css">Not Available</div>`
-                    document.getElementById('blog-text').style.opacity = "50%"
-                    document.querySelector(".img-blog").style.opacity = "50%"
-                }
-                else {
-                    document.getElementById('blog-text').innerHTML = `<a href="${data.blog}" <div class="blog-text-css">${data.blog}</div></a>`
-                    document.getElementById('blog-text').style.opacity = "100%"
-                    document.querySelector(".img-blog").style.opacity = "100%"
-                }
-
-                if (data.twitter_username == null) {
-                    document.getElementById('twitter-text').innerHTML = `<div class="twitter-text-css">Not Available</div>`
-                    document.getElementById('twitter-text').style.opacity = "50%"
-                    document.querySelector(".img-twitter").style.opacity = "50%"
-                }
-                else {
-                    document.getElementById('twitter-text').innerHTML = `<a href="https://twitter.com/${data.twitter_username}" <div class="twitter-text-css">@${data.twitter_username}</div></a>`
-                    document.getElementById('twitter-text').style.opacity = "100%"
-                    document.querySelector(".img-twitter").style.opacity = "100%"
-                }
-
-                if (data.company == null) {
-                    document.getElementById('company-text').innerHTML = `<div class="company-text-css">Not Available</div>`
-                    document.getElementById('company-text').style.opacity = "50%"
-                    document.querySelector(".img-company").style.opacity = "50%"
-                }
-                else {
-                    document.getElementById('company-text').innerHTML = `<a href="https://github.com/${data.company.slice(1)}" <div class="company-text-css">${data.company}</div></a>`
-                    document.getElementById('company-text').style.opacity = "100%"
-                    document.querySelector(".img-company").style.opacity = "100%"
-                }
-            }
-            else {
+            if (!data.login) {
                 document.querySelector(".error-message").style.display = "block"
+                return;
             }
 
-        })
+            updateDateJoined(data)
+            document.querySelector(".error-message").style.display = "none"
+            document.getElementById('picture').innerHTML = `<img class="picture-css" src="${data.avatar_url}"/>`
+            document.getElementById('name').innerHTML = `<h1 class="name-css">${data.name ? data.name : data.login}</h1>`
+            document.getElementById('username').innerHTML = `<div class="username-css">@${data.login}</div>`
+            document.getElementById('bio').innerHTML = `<div class="bio-css">${data.bio ? data.bio : "This profile has no bio"}</div>`
+            document.getElementById('bio').style.opacity = `${data.bio ? "100%" : "50%"}`
+            document.getElementById('repos').innerHTML = `<h1 class="repos-css">${data.public_repos}</h1>`
+            document.getElementById('followers').innerHTML = `<h1 class="repos-css">${data.followers}</h1>`
+            document.getElementById('following').innerHTML = `<h1 class="repos-css">${data.following}</h1>`
+            document.getElementById('location-text').innerHTML = `<div class="location-text-css">${data.location ? data.location : "Not Available"}</div>`
+            document.getElementById('location-text').style.opacity = `${data.location ? "100%" : "50%"}`
+            document.querySelector('.img-location').style.opacity = `${data.location ? "100%" : "50%"}`
+            document.getElementById('blog-text').innerHTML = `<a href="${data.blog}" <div class="blog-text-css">${data.blog ? data.blog : "Not Available"}</div></a>`
+            document.getElementById('blog-text').style.opacity = `${data.blog ? "100%" : "50%"}`
+            document.getElementById('blog-text').style.pointerEvents = `${data.blog ? "all" : "none"}`
+            document.querySelector('.img-blog').style.opacity = `${data.blog ? "100%" : "50%"}`
+            document.getElementById('twitter-text').innerHTML = `<a href="https://twitter.com/${data.twitter_username}" <div class="twitter-text-css">${data.twitter_username ? data.twitter_username : "Not Available"}</div></a>`
+            document.getElementById('twitter-text').style.pointerEvents = `${data.twitter_username ? "all" : "none"}`
+            document.getElementById('twitter-text').style.opacity = `${data.twitter_username ? "100%" : "50%"}`
+            document.querySelector('.img-twitter').style.opacity = `${data.twitter_username ? "100%" : "50%"}`
+            document.getElementById('company-text').innerHTML = data.company ? `<a href="https://github.com/${data.company.slice(1)}" <div class="company-text-css">${data.company}</div></a>` : `<div class="company-text-css">Not Available</div>`
+            document.getElementById('company-text').style.pointerEvents = `${data.company ? "all" : "none"}`
+            document.getElementById('company-text').style.opacity = `${data.company ? "100%" : "50%"}`
+            document.querySelector('.img-company').style.opacity = `${data.company ? "100%" : "50%"}`
+        }
+    )
 }
 
 let form = document.getElementById("form")
@@ -105,7 +66,8 @@ const toggleSwitch = document.querySelector('.switch-container')
 document.documentElement.setAttribute('theme', 'light');
 
 function switchTheme() {
-    if (document.documentElement.getAttribute('theme') == 'light') {
+    let theme = document.documentElement.getAttribute('theme')
+    if (theme == 'light') {
         document.documentElement.setAttribute('theme', 'dark');
         document.querySelector(".theme").innerHTML = "LIGHT"
 
@@ -117,6 +79,6 @@ function switchTheme() {
 
 }
 
-toggleSwitch.addEventListener('click', switchTheme, false);
+toggleSwitch.addEventListener('click', switchTheme);
 
 
